76120aed0eecb29251cf84d6ec82a2b0a4397924
OliRae <olivier.vanraemdonck@mobius.eu>
2020-11-28 12:32:47 +0100

    General cleanup & enable script run

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # import the necessary packages
+import cv2
 from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
-import cv2
 
-# define the dictionary of digit segments so we can identify
-# each digit on the thermostat
+# define the dictionary of digit segments so we can identify each digit on the thermostat
 DIGITS_LOOKUP = {
     (1, 1, 1, 0, 1, 1, 1): 0,
     (0, 0, 1, 0, 0, 1, 0): 1,
@@ -21,76 +20,91 @@ DIGITS_LOOKUP = {
 
 # load the example image
 image = cv2.imread("examples/thermostat.jpg")
+cv2.imwrite("steps/step_0_original.jpg", image)
 
-# pre-process the image by resizing it, converting it to
-# graycale, blurring it, and computing an edge map
+# pre-process the image by resizing it, converting it to graycale, blurring it, and computing an edge map
 image = imutils.resize(image, height=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("steps/step_1_grayed.jpg", gray)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+cv2.imwrite("steps/step_2_blurred.jpg", blurred)
 edged = cv2.Canny(blurred, 50, 200, 255)
+cv2.imwrite("steps/step_3_edged.jpg", edged)
 
-# find contours in the edge map, then sort them by their
-# size in descending order
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-                        cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+# find contours in the edge map, then sort them by their size in descending order
+contours = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
 displayCnt = None
 
 # loop over the contours
-for c in cnts:
+for c in contours:
+
     # approximate the contour
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-    # if the contour has four vertices, then we have found
-    # the thermostat display
+    # if the contour has four vertices, then we have found the thermostat display
     if len(approx) == 4:
         displayCnt = approx
         break
 
-# extract the thermostat display, apply a perspective transform
-# to it
+# extract the thermostat display, apply a perspective transform to it
 warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+cv2.imwrite("steps/step_4_display.jpg", warped)
 output = four_point_transform(image, displayCnt.reshape(4, 2))
 
-# threshold the warped image, then apply a series of morphological
-# operations to cleanup the thresholded image
+# threshold the warped image, then apply a series of morphological operations to cleanup the thresholded image
 thresh = cv2.threshold(warped, 0, 255,
                        cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+cv2.imwrite("steps/step_5_negative.jpg", thresh)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+cv2.imwrite("steps/step_6_smoothen.jpg", thresh)
 
-# find contours in the thresholded image, then initialize the
-# digit contours lists
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                        cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-digitCnts = []
+# find contours in the thresholded image, then initialize the digit contours lists
+contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contoursOfDigits = []
 
 # loop over the digit area candidates
-for c in cnts:
+for c in contours:
+
     # compute the bounding box of the contour
     (x, y, w, h) = cv2.boundingRect(c)
 
-    # if the contour is sufficiently large, it must be a digit
-    print('w,h ='), w, h
-    cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
-    if w >= 15 and (h >= 30 and h <= 40):
-        digitCnts.append(c)
+    # Draw a red box around all digit area candidates
+    cv2.rectangle(output, (x, y), (x + w, y + h), (0, 0, 255), 1)
 
-# sort the contours from left-to-right, then initialize the
-# actual digits themselves
-# digitCnts = contours.sort_contours(digitCnts,
-#	method="left-to-right")[0]
+    # if the contour is sufficiently large, it must be a digit
+    if w >= 15 and (h >= 30 and h <= 40):
+        contoursOfDigits.append(c)
+
+        # Draw a green box around the contours where a digit is
+        cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+cv2.imwrite("steps/step_7_contours.jpg", output)
+
+# sort the contours from left-to-right, then initialize the actual digits themselves
 digits = []
 
+# Initialize counter
+digit_count = 1
+
 # loop over each of the digits
-print('digitCnts'), digitCnts
-for c in digitCnts:
-    # extract the digit ROI
+for c in contoursOfDigits:
+
+    # extract the digit ROI (region of interest)
     (x, y, w, h) = cv2.boundingRect(c)
     roi = thresh[y:y + h, x:x + w]
+
+    # Increase counter
+    digit_count = digit_count + 1
+
+    # Write out image of digit ROI
+    cv2.imwrite("steps/step_8_RIO_" + str(digit_count) + ".jpg", roi)
 
     # compute the width and height of each of the 7 segments
     # we are going to examine
@@ -110,19 +124,14 @@ for c in digitCnts:
     ]
     on = [0] * len(segments)
 
-    # sort the contours from left-to-right, then initialize the
-    # actual digits themselves
-    # loop over the segments
+    # sort the contours from left-to-right, then initialize the actual digits themselves loop over the segments
     for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-        # extract the segment ROI, count the total number of
-        # thresholded pixels in the segment, and then compute
-        # the area of the segment
+        # extract the segment ROI, count the total number of thresholded pixels in the segment, and then compute the area of the segment
         segROI = roi[yA:yB, xA:xB]
         total = cv2.countNonZero(segROI)
         area = (xB - xA) * (yB - yA)
 
-        # if the total number of non-zero pixels is greater than
-        # 50% of the area, mark the segment as "on"
+        # if the total number of non-zero pixels is greater than 50% of the area, mark the segment as "on"
         if total / float(area) > 0.5:
             on[i] = 1
 
@@ -133,9 +142,4 @@ for c in digitCnts:
     cv2.putText(output, str(digit), (x - 10, y - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
 
-# display the digits
-print(digits)
-# print(u"{}{}.{} \u00b0C".format(*digits))
-cv2.imshow("Input", image)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+cv2.imwrite("steps/step_9_result.jpg", output)
