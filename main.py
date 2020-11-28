c30eb9dea8ce08933aad1a229e26fe0513e97e09
OliRae <olivier.vanraemdonck@mobius.eu>
2020-11-28 18:07:03 +0100

    give variables understandable names

--- main.py
+++ main.py
@@ -11,36 +11,37 @@ import functions as f
 image_original = f.read_image("examples/thermostat.jpg")
 
 # step 1: resize the image
-image = f.resize_image(image=image_original, image_height=500)
+image_resized = f.resize_image(image=image_original, image_height=500)
 
 # step 2: make image gray
-gray = f.gray_image(image=image)
+image_grayed = f.gray_image(image=image_resized)
 
 # step 3: blur image
-blurred = f.blur_image(
-    image=gray, kernel_size=(5, 5), sigma_x=0, sigma_y=0)
+image_blurred = f.blur_image(
+    image=image_grayed, kernel_size=(5, 5), sigma_x=0, sigma_y=0)
 
 # step 4: get image edges
-edged = f.get_image_edges(image=blurred, threshold_1=50,
-                          threshold_2=200, edges=255)
+image_edged = f.get_image_edges(image=image_blurred, threshold_1=50,
+                                threshold_2=200, edges=255)
 
 # step 5: find display
-warped, output = f.extract_display(
-    image_resized=image, image_grayed=gray, image_edged=edged)
+display_grayed, display = f.extract_display(
+    image_resized=image_resized, image_grayed=image_grayed, image_edged=image_edged)
 
 # step 6: make display binary (black or white)
-thresh = f.make_pixels_black_or_white(warped, threshold=0, maximum_value=255)
+display_binary = f.make_pixels_black_or_white(
+    display_grayed, threshold=0, maximum_value=255)
 
 # step 7: remove noise from display
-thresh = f.remove_noise_from_image(
-    thresh, shape=cv2.MORPH_ELLIPSE, size=(1, 5))
+display_binary_sharp = f.remove_noise_from_image(
+    display_binary, shape=cv2.MORPH_ELLIPSE, size=(1, 5))
 
 # step 8: find digit areas
 contoursOfDigits = f.find_digit_areas(
-    binary_display_without_noise=thresh, display=output, min_width_digit_area=15, max_width_digit_area=30, min_height_digit_area=30, max_height_digit_area=40)
+    binary_display_without_noise=display_binary_sharp, display=display, min_width_digit_area=15, max_width_digit_area=30, min_height_digit_area=30, max_height_digit_area=40)
 
 # step 9: read digits
 result = f.read_digits(
-    binary_display_without_noise=thresh, display=output, contours_of_digits=contoursOfDigits, alpha=0.25, beta=0.15, gamma=0.05, min_fill_area=0.5)
+    binary_display_without_noise=display_binary_sharp, display=display, contours_of_digits=contoursOfDigits, alpha=0.25, beta=0.15, gamma=0.05, min_fill_area=0.5)
 
 print(result)
