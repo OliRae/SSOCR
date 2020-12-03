d21ea665ed741ccb4a03fea9c959fdb2e659f205
OliRae <olivier.vanraemdonck@mobius.eu>
2020-12-03 21:58:23 +0100

    add documentation / improve black-and-white rendering

--- functions.py
+++ functions.py
@@ -6,7 +6,15 @@ import imutils
 
 
 def read_image(path):
-    # step 0: read original image
+    """
+    loads an image
+
+    Args:
+        path (str): path to image
+
+    Returns:
+        object: image
+    """
 
     # load the example image
     image = cv2.imread(path)
@@ -17,13 +25,22 @@ def read_image(path):
     return image
 
 
-def resize_image(image, image_height=500, estimated_display_region=None):
-    # step 1: resize the image
+def resize_image(image, image_height=500):
+    """
+    Resize image so image can be made smaller. Larger images are often easier to work with because they hold more pixels.
+
+    Args:
+        image (object): image to resize
+        image_heigt (int): desired height of image
+
+    Returns:
+        object: image with given height
+    """
 
     # cut to estimated region
-    if estimated_display_region is not None:
-        image = image[estimated_display_region[0]:estimated_display_region[2],
-                      estimated_display_region[1]:estimated_display_region[3]].copy()
+    # if estimated_display_region is not None:
+    #     image = image[estimated_display_region[0]:estimated_display_region[2],
+    #                   estimated_display_region[1]:estimated_display_region[3]].copy()
 
     # resize image
     image_resized = imutils.resize(image=image, height=image_height)
@@ -35,7 +52,16 @@ def resize_image(image, image_height=500, estimated_display_region=None):
 
 
 def gray_image(image):
-    # step 2: convert to greyscale
+    """
+    Converts color image to greyscale
+
+    Args:
+        image (object): image to convert to greyscale
+        image_heigt (int): desired height of image
+
+    Returns:
+        object: grey image
+    """
 
     # make image gray
     image_grayed = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
@@ -46,12 +72,22 @@ def gray_image(image):
     return image_grayed
 
 
-def blur_image(image, kernel_size=(5, 5), sigma_x=0, sigma_y=0):
-    # step 3: blur image to remove noise from image
+def blur_image(image, kernel_size_width=5, kernel_size_height=5):
+    """
+    Blurs image. Increase kernel sizes to make image more blurry. Kernel sizes define a rectange of pixels of which the average color is taken
+
+    Args:
+        image (object): image to blur
+        kernel_size_width (int): width of rectange in pixels
+        kernel_size_height (int): height of rectange in pixels
+
+    Returns:
+        object: blurred image
+    """
 
     # make image blurry
     image_grayed = cv2.GaussianBlur(
-        src=image, ksize=kernel_size, sigmaX=0, sigmaY=0)
+        src=image, ksize=(kernel_size_width, kernel_size_height), sigmaX=0, sigmaY=0)
 
     # write out image
     cv2.imwrite("steps/step_3_blurred.jpg", image_grayed)
@@ -60,11 +96,22 @@ def blur_image(image, kernel_size=(5, 5), sigma_x=0, sigma_y=0):
 
 
 def get_image_edges(image, threshold_1=50, threshold_2=200, edges=255):
-    # step 4: get edges in the image
+    """
+    Detect edges in image.
+
+    Args:
+        image (object): image to detect edges in
+        threshold_1 (int):
+        threshold_2 (int):
+        edges (int):
+
+    Returns:
+        object: image of edges
+    """
 
     # get edges
     image_edges = cv2.Canny(
-        image=image, threshold1=threshold_1, threshold2=threshold_2, edges=edges)
+        image=image, threshold1=threshold_1, threshold2=threshold_2, edges=edges, L2gradient=True)
 
     # write out image
     cv2.imwrite("steps/step_4_edges.jpg", image_edges)
@@ -72,8 +119,21 @@ def get_image_edges(image, threshold_1=50, threshold_2=200, edges=255):
     return image_edges
 
 
-def extract_display(image_resized, image_grayed, image_edged, epsilon_factor=0.02, width_display=[650, 850], height_display=[250, 350]):
-    # step 5: find contours in the edge map
+def extract_display(image_resized, image_grayed, image_edged, accuracy=0.02, width_display=[650, 850], height_display=[250, 350]):
+    """
+    Extract the display from the image
+
+    Args:
+        image_resized (object): image with specific height
+        image_grayed (object): image in greyscale
+        image_edged (object): edges of image
+        accuracy (dbl): parameter specifying the approximation accuracy
+        width_display (int[min, max]): minimum and maximum width of display
+        height_display (int[min, max]): minimum and maximum height of display
+
+    Returns:
+        object: image with rectange around display, image of display
+    """
 
     # get contours in edge map
     contours = cv2.findContours(image_edged.copy(), cv2.RETR_EXTERNAL,
@@ -82,21 +142,20 @@ def extract_display(image_resized, image_grayed, image_edged, epsilon_factor=0.0
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     countoursOfDisplay = None
 
+    # make copy of image to draw annotations on
+    image_annotated = image_resized.copy()
+
     # loop over the contours
     for c in contours:
 
         # approximate the contour
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(
-            curve=c, epsilon=epsilon_factor*peri, closed=True)
+            curve=c, epsilon=accuracy*peri, closed=True)
 
-        # draw rectangle around the contour
+        # draw a boudnig rectange around the vertices
         (x, y, w, h) = cv2.boundingRect(approx)
-        cv2.rectangle(image_resized, (x, y),
-                      (x + w, y + h), (255, 0, 0), 3)
 
-        # print((x, y, w, h), len(approx) == 4 and w >= width_display[0] and w <= width_display[1]
-        #       and h >= height_display[0] and h <= height_display[1])
         # if the contour has four vertices, then we have found the display
         # display must be within min and max sizes
         if len(approx) == 4 and w >= width_display[0] and w <= width_display[1] and h >= height_display[0] and h <= height_display[1]:
@@ -104,14 +163,35 @@ def extract_display(image_resized, image_grayed, image_edged, epsilon_factor=0.0
             countoursOfDisplay = approx
 
             # draw rectangle around the display
-            (x, y, w, h) = cv2.boundingRect(approx)
-            cv2.rectangle(image_resized, (x, y),
+            cv2.rectangle(image_annotated, (x, y),
                           (x + w, y + h), (0, 255, 0), 5)
 
+            # draw contour points
+            cv2.drawContours(image=image_annotated, contours=approx,
+                             contourIdx=-1, color=(0, 0, 255), thickness=5)
+
+            # annotate rectangle with dimensions
+            cv2.putText(img=image_annotated, text=('vertices: '+str(len(approx))+' | dim: '+str(w)+' x '+str(h)), org=(x, y-10),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 255, 0))
+
             break
+        else:
+            # if conditions are not met, just annotate the candidate
+
+            # draw rectangle around the contour
+            cv2.rectangle(image_annotated, (x, y),
+                          (x + w, y + h), (255, 0, 0), 3)
+
+            # draw contour points
+            cv2.drawContours(image=image_annotated, contours=approx,
+                             contourIdx=-1, color=(2505, 0, 255), thickness=5)
+
+            # annotate rectangle with dimensions
+            cv2.putText(img=image_annotated, text=('vertices: '+str(len(approx))+' | dim: '+str(w)+' x '+str(h)), org=(x, y-10),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4, color=(0, 255, 0))
 
     # draw rectangle around the display
-    cv2.imwrite("steps/step_5_display_full.jpg", image_resized)
+    cv2.imwrite("steps/step_5_display_full.jpg", image_annotated)
 
     # extract the thermostat display, apply a perspective transform to it
     image_display_grayed = four_point_transform(
@@ -126,17 +206,47 @@ def extract_display(image_resized, image_grayed, image_edged, epsilon_factor=0.0
         image=image_display, height=500)
 
     # write out image
-    cv2.imwrite("steps/step_5_display.jpg", image_display)
+    cv2.imwrite("steps/step_5_display.jpg", image_display_grayed)
 
     return image_display_grayed, image_display
 
 
-def make_pixels_black_or_white(image, threshold=0):
-    # step 6: make image negative
+def increase_contrast(image, threshold=2, size=(6, 6)):
+    """
+    Increase contrast of image
 
-    # For every pixel, the same threshold value is applied. If the pixel value is smaller than the threshold, it is set to 0, otherwise it is set to a maximum value.
-    binary_image = cv2.threshold(src=image, thresh=threshold, maxval=255,
-                                 type=cv2.THRESH_BINARY_INV)[1]
+    Args:
+        threshold (int): bigger number is results in higher contrast
+        size (int, int): bigger number is results in higher contrast
+
+    Returns:
+        object: image with increased contrast
+    """
+    clahe = cv2.createCLAHE(clipLimit=threshold, tileGridSize=size)
+    image_contrast = clahe.apply(image)
+
+    # write out image
+    cv2.imwrite("steps/step_6_contrast.jpg",
+                image_contrast)
+
+    return image_contrast
+
+
+def make_pixels_black_or_white(image, threshold_1=0, threshold_2=0):
+    """
+    Make image binary (black or white)
+
+    Args:
+        threshold_1 (int): higher number increases black/white
+        threshold_2 (int): higher number increases black/white
+
+    Returns:
+        object: binary image
+    """
+
+    # apply thresholding
+    binary_image = cv2.adaptiveThreshold(src=image, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                         thresholdType=cv2.THRESH_BINARY_INV, blockSize=threshold_1, C=threshold_2)
 
     # write out image
     cv2.imwrite("steps/step_6_black_or_white_pixels.jpg",
@@ -146,35 +256,64 @@ def make_pixels_black_or_white(image, threshold=0):
 
 
 def remove_noise_from_image(binary_image, shape_opening=cv2.MORPH_ELLIPSE, shape_closing=cv2.MORPH_ELLIPSE, size_opening=(1, 5), size_closing=(1, 5)):
-    # step 7: remove noise from image
+    """
+    Make image binary (black or white)
+
+    Args:
+        image (object): a greyscale image
+        shape_opening:
+        shape_closing:
+        size_opening:
+        size_closing:
+
+    Returns:
+        object: sharp image
+    """
+    image = binary_image.copy()
 
     # define structuring element for opening
     kernel_opening = cv2.getStructuringElement(
         shape=shape_opening, ksize=size_opening)
 
     # remove noise
-    image_opening = cv2.morphologyEx(
-        src=binary_image, op=cv2.MORPH_OPEN, kernel=kernel_opening)
+    image = cv2.morphologyEx(
+        src=image, op=cv2.MORPH_OPEN, kernel=kernel_opening)
 
     # write out image
-    cv2.imwrite("steps/step_7_opening.jpg", image_opening)
+    cv2.imwrite("steps/step_7_1_opening.jpg", image)
 
-    # define structuring element for opening
+    # define structuring element for closing
     kernel_closing = cv2.getStructuringElement(
         shape=shape_closing, ksize=size_closing)
 
     # close small holes inside the foreground objects, or small black points on the object
-    image_closing = cv2.morphologyEx(
-        src=image_opening, op=cv2.MORPH_CLOSE, kernel=kernel_closing)
+    image = cv2.morphologyEx(
+        src=image, op=cv2.MORPH_CLOSE, kernel=kernel_closing)
 
     # write out image
-    cv2.imwrite("steps/step_7_closing.jpg", image_closing)
+    cv2.imwrite("steps/step_7_2_closing.jpg", image)
 
-    return image_closing
+    return image
 
 
 def find_digit_areas(binary_display_without_noise, display, min_width_digit_area=15, max_width_digit_area=30, min_height_digit_area=30, max_height_digit_area=40):
-    # step 8: find digit areas
+    """
+    Find areas of digits
+
+    Args:
+        binary_display_without_noise (object): image of display without noise
+        display (object): image of display
+        min_width_digit_area:
+        max_width_digit_area:
+        min_height_digit_area:
+        max_height_digit_area:
+
+    Returns:
+        list: contours of digit areas
+    """
+
+    # make a copy of the display where annotations can be added to
+    image_annotated = display.copy()
 
     # find contours in the thresholded image, then initialize the digit contours lists
     contours = cv2.findContours(image=binary_display_without_noise.copy(
@@ -188,24 +327,62 @@ def find_digit_areas(binary_display_without_noise, display, min_width_digit_area
         # compute the bounding box of the contour
         (x, y, w, h) = cv2.boundingRect(c)
 
-        # draw a blue box around all digit area candidates with
-        cv2.rectangle(display, (x, y), (x + w, y + h), (255, 0, 0), 1)
-
         # if the contour is sufficiently large, it must be a digit
         if (w >= min_width_digit_area and w <= max_width_digit_area) and (h >= min_height_digit_area and h <= max_height_digit_area):
+
+            # the contour is a digit
             contoursOfDigits.append(c)
 
-            # Draw a thick blue box around the contours where a digit is
-            cv2.rectangle(display, (x, y), (x + w, y + h),
+            # draw contour points
+            cv2.drawContours(image=image_annotated, contours=c,
+                             contourIdx=-1, color=(0, 0, 255), thickness=5)
+
+            # draw a thick blue box around the contours where a digit is
+            cv2.rectangle(image_annotated, (x, y), (x + w, y + h),
                           (255, 0, 0), 3)
 
-    cv2.imwrite("steps/step_8_digit_areas.jpg", display)
+            # annotate rectangle with dimensions
+            cv2.putText(img=image_annotated, text=('dim: '+str(w)+' x '+str(h)), org=(x, y-10),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255))
+
+        else:
+
+            # draw a blue box around digit area candidate
+            cv2.rectangle(image_annotated, (x, y),
+                          (x + w, y + h), (255, 0, 0), 1)
+
+            # draw contour points
+            cv2.drawContours(image=image_annotated, contours=c,
+                             contourIdx=-1, color=(0, 0, 255), thickness=5)
+
+            # annotate rectangle with dimensions
+            cv2.putText(img=image_annotated, text=('dim: '+str(w)+' x '+str(h)), org=(x, y-10),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 0, 255))
+
+    cv2.imwrite("steps/step_8_digit_areas.jpg", image_annotated)
 
     return contoursOfDigits
 
 
 def read_digits(binary_display_without_noise, display, contours_of_digits, alpha=0.25, beta=0.15, gamma=0.05, min_fill_area=0.5, min_width_digit_1=20, max_width_digit_1=60, bottom_right_segment_offset_to_left=5):
+    """
+    Read digits on display
 
+    Args:
+        binary_display_without_noise (object): image of display without noise
+        display (object): image of display
+        contours_of_digits:
+        alpha: width of vertical segment / width of ROI
+        beta: height of horizontal segment / height of ROI
+        gamma: half the height of the horizontal center segment / height of ROI
+        min_fill_area: minium % of segment that must be filled in order to be considered as on
+        min_width_digit_1:
+        max_width_digit_1:
+        bottom_right_segment_offset_to_left:
+
+    Returns:
+        array of integers
+    """
     # w = width of ROI
     # dw = width of vertical segment
     # h = height of ROI
@@ -214,6 +391,9 @@ def read_digits(binary_display_without_noise, display, contours_of_digits, alpha
     # alpha = dw / w
     # beta = dh / h
     # gamma = dHC / h
+
+    # make a copy of the image to annotate
+    display_annotated = display.copy()
 
     # sort the contours from left-to-right
     contours_of_digits = contours.sort_contours(
@@ -264,7 +444,7 @@ def read_digits(binary_display_without_noise, display, contours_of_digits, alpha
             DIGITS_LOOKUP = {
                 (1, 1, 1, 0, 1, 1, 1): 0,
                 (0, 0, 1, 0, 0, 1, 0): 1,
-                (1, 0, 1, 1, 1, 1, 0): 2,
+                (1, 0, 1, 1, 1, 0, 1): 2,
                 (1, 0, 1, 1, 0, 1, 1): 3,
                 (0, 1, 1, 1, 0, 1, 0): 4,
                 (1, 1, 0, 1, 0, 1, 1): 5,
@@ -314,18 +494,31 @@ def read_digits(binary_display_without_noise, display, contours_of_digits, alpha
         # lookup the digit and draw it on the image
         digit = DIGITS_LOOKUP[tuple(on)]
         digits.append(digit)
-        cv2.putText(display, str(digit), (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 2)
+        cv2.putText(img=display_annotated, text=str(digit), org=(x+(w//4), y+(h)),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=7, color=(0, 0, 255), thickness=20)
 
     # Make image bigger
     # display = resize_image(display, image_height=300)
-    display = imutils.resize(
-        image=display, height=300)
+    display_annotated = imutils.resize(
+        image=display_annotated, height=300)
 
     # Write out image
-    cv2.imwrite("steps/result.jpg", display)
-
-    # cv2.imshow(winname="Result is " + str(digits), mat=display)
-    # cv2.waitKey()
+    cv2.imwrite("steps/result.jpg", display_annotated)
 
     return digits
+
+
+def convert_to_number(array):
+    """
+    converts arrays of integer to a single number.
+    e.g. [1, 2, 3] becomes 123
+    """
+    # convert digits array to single number
+    result = 0
+
+    for d in list(reversed(array)):
+
+        multiplicator = 10 ** (list(reversed(array)).index(d))
+        result += d * multiplicator
+
+    return(result)
--- main.py
+++ main.py
@@ -8,41 +8,47 @@ import imutils
 import functions as f
 
 # step 0: read the image
-image_original = f.read_image("examples/peter_5.jpg")
+image_original = f.read_image("training_data_set_2/IMG_5289.JPG")
 
 # step 1: resize the image
 image_resized = f.resize_image(
-    image=image_original, image_height=500, estimated_display_region=None)
+    image=image_original, image_height=500)
 
 # step 2: make image gray
 image_grayed = f.gray_image(image=image_resized)
 
 # step 3: blur image
 image_blurred = f.blur_image(
-    image=image_grayed, kernel_size=(5, 5), sigma_x=0, sigma_y=0)
+    image=image_grayed, kernel_size_width=7, kernel_size_height=7)
 
 # step 4: get image edges
-image_edged = f.get_image_edges(image=image_blurred, threshold_1=50,
-                                threshold_2=200, edges=255)
+image_edged = f.get_image_edges(image=image_blurred, threshold_1=0,
+                                threshold_2=150, edges=255)
 
 # step 5: find display
 display_grayed, display = f.extract_display(
-    image_resized=image_resized, image_grayed=image_grayed, image_edged=image_edged, width_display=[300, 400], height_display=[80, 120])
+    image_resized=image_resized, image_grayed=image_grayed, image_edged=image_edged, accuracy=0.05, width_display=[190, 200], height_display=[65, 75])
+
+# step 6: increase contrast of image
+display_contrast = f.increase_contrast(
+    display_grayed, threshold=1, size=(10, 10))
 
 # step 6: make display binary (black or white)
 display_binary = f.make_pixels_black_or_white(
-    display_grayed, threshold=50)
+    display_contrast, threshold_1=291, threshold_2=23)
 
 # step 7: remove noise from display
 display_binary_sharp = f.remove_noise_from_image(
-    display_binary, shape_opening=cv2.MORPH_ELLIPSE, shape_closing=cv2.MORPH_ELLIPSE, size_opening=(1, 1), size_closing=(1, 50))
+    display_binary, shape_opening=cv2.MORPH_ELLIPSE, shape_closing=cv2.MORPH_ELLIPSE, size_opening=(1, 1), size_closing=(31, 31))
 
 # step 8: find digit areas
 contoursOfDigits = f.find_digit_areas(
-    binary_display_without_noise=display_binary_sharp, display=display, min_width_digit_area=50, max_width_digit_area=200, min_height_digit_area=200, max_height_digit_area=380)
+    binary_display_without_noise=display_binary_sharp, display=display, min_width_digit_area=60, max_width_digit_area=275, min_height_digit_area=320, max_height_digit_area=400)
 
 # step 9: read digits
-result = f.read_digits(
-    binary_display_without_noise=display_binary_sharp, display=display, contours_of_digits=contoursOfDigits, alpha=0.25, beta=0.15, gamma=0.05, min_fill_area=0.4, min_width_digit_1=20, max_width_digit_1=100, bottom_right_segment_offset_to_left=20)
+digits = f.read_digits(
+    binary_display_without_noise=display_binary_sharp, display=display, contours_of_digits=contoursOfDigits, alpha=0.25, beta=0.15, gamma=0.05, min_fill_area=0.4, min_width_digit_1=60, max_width_digit_1=100, bottom_right_segment_offset_to_left=20)
+
+result = f.convert_to_number(digits)
 
 print(result)
